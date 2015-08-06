9f672c4619c092b8ad914ef0a256085ecf1aa0ac
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-06 11:09:57 -0700

    (feat) Create package.json and Mongoos Schemas for diningtable, guest, user

--- app-db/diningTbl/diningTblModel.js
+++ app-db/diningTbl/diningTblModel.js
@@ -1,1 +1,19 @@
 /* create and export diningTbl schema and model */
+var mongoose = require('mongoose');
+var DiningTableSchema = mongoose.Schema({
+  diningTablename: String,
+  guestsAtTable: Array,
+  seatsPerTable: Number
+});
+
+var DiningTables = mongoose.model('DiningTables', DiningTableSchema);
+
+module.exports = DiningTables;
+
+
+
+
+// DiningTablename
+// DT id 
+// guest Array
+// number of seats
--- app-db/guests/guestModel.js
+++ app-db/guests/guestModel.js
@@ -1,1 +1,21 @@
 /* create and export guest schema */
+var mongoose = require('mongoose');
+var GuestSchema = mongoose.Schema({
+  guestName: String,
+  friendId: Number,
+  diningTableId: Number,
+  constraints: Array
+});
+
+var Guests = mongoose.model('Guests', GuestSchema);
+
+module.exports = Guests;
+
+
+
+
+//guest name 
+//guest id (mongoose adds it by default)
+// plus 1 id
+//dining table id
+// constraints
