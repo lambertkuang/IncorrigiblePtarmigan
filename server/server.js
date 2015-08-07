58a8306d85f0f1109f55985a5bcc73f26f27f8df
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-06 19:22:01 -0700

    Server can post a list of guests and retrieve it on routing rule for /create

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
 /* this will be our express server */
+
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+mongoose.connect('mongodb://localhost/seatly');
+
+require('./router.js')(app, express);
+
+app.listen(8000);
+
+module.exports = app;
+
+
+// app.get('/', function(req, res) {
+//   res.send('Hellooooo WOOOOOORLD!!!!');
+// });
