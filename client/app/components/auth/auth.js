52a7f272e8ce4cd484e7a5d33b608eeeab182754
Lambert <lambertkuang@gmail.com>
2015-08-07 11:17:52 -0700

    Add signup and signin views and controllers, changed file structure

---
+++ client/app/components/auth/auth.js
@@ -0,0 +1,29 @@
+angular.module('seatly.auth', [])
+
+.controller('AuthController', function($scope, $window, $location, Auth) {
+  $scope.user = {};
+
+  $scope.signin = function() {
+    Auth.signin($scope.user)
+      .then(function(token) {
+        $window.localStorage.setItem('com.seatly', token);
+        // maybe need to update path once user signs in
+        $location.path('/');
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function() {
+    Auth.signup($scope.user)
+      .then(function(token) {
+        $window.localStorage.setItem('com.seatly', token);
+        // maybe need to update path once user signs in
+        $location.path('/');
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+});
