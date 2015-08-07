40d3f93a4589ecc22fdc28f76fe927ce0dee998d
batmanimal <thejenniekim@gmail.com>
2015-08-07 11:58:04 -0700

    Create guest input view html

---
+++ client/app/components/guestInput/guestInputFactory.js
@@ -0,0 +1,15 @@
+angular.module('seatly.guestInput', [])
+.factory('guestInputFactory', function($http){
+	var addAllGuests = function(guestsArray){
+		return $http({
+			method: 'POST',
+			url: '/create',
+			data: guestsArray
+		});
+	};
+
+	return {
+		'addAllGuests': addAllGuests
+	};
+
+});
