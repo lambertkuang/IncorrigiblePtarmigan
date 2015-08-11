8479698b5b29b2c5ccfacd329c2fbd7b0ab59d94
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-11 12:07:08 -0700

    (refactor) comment and clean code up

--- client/app/components/edit/editController.js
+++ client/app/components/edit/editController.js
@@ -1,5 +1,6 @@
 angular.module('seatly.edit', [])
 .controller('editController', function($scope, Edit) {
+  // TODO turn this into a real guest, not a hardcoded example
   $scope.guest = {
     guestName: 'example',
     friendName: 'friend!',
@@ -8,12 +9,16 @@ angular.module('seatly.edit', [])
   };
 
   $scope.editGuest = function()  {
+    // format the information in the way the server expects
+    // TODO only provide the information that has changed
     var obj = {
       changes: [$scope.guest.guestName, $scope.guest]
     };
 
+    // pass object through to factory
     return Edit.editGuest(obj)
-    .then(function(res) {
+    // no matter if error or not, clear fields
+    .finally(function(res) {
       $scope.guest = {
         guestName: '',
         friendName: '',
