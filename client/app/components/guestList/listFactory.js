30de43e356e571e9c76d277ac898c0357e193ebf
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-11 15:39:16 -0700

    (refactor) squish edit and guestList into one factory/controller/view in order to pass information properly between the two

--- client/app/components/guestList/listFactory.js
+++ client/app/components/guestList/listFactory.js
@@ -7,8 +7,29 @@ angular.module('seatly.list')
     });
   };
 
+  var getGuest = function(guestName) {
+    $http({
+      method: 'POST'
+    }).then(function(guest) {
+
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  };
+
+  var editGuest = function(guestObj) {
+    return $http({
+      method: 'PUT',
+      url: '/edit',
+      data: guestObj
+    });
+  };
+
   return {
-    'viewGuests': viewGuests
+    'viewGuests': viewGuests,
+    'getGuest': getGuest,
+    'editGuest': editGuest
   };
 
 });
