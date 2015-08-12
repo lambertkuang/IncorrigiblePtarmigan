91b03c4599006f01ab13ac3436deaf6e798904fd
Lambert <lambertkuang@gmail.com>
2015-08-12 16:54:46 -0700

    Add decode function to decode user token

--- server/utils/utility.js
+++ server/utils/utility.js
@@ -1,1 +1,21 @@
-/* this will be a file for all of our utility logic and functions for server-side code */
+var jwt = require('jwt-simple');
+
+
+// decodes the token to identify the user and appends the information to the request
+module.exports = {
+  decode: function(req, res, next) {
+    var token = req.headers['x-access-token'];
+    var user;
+    if (!token) {
+      return res.send(403); //send forbidden if token is not provided
+    }
+    try {
+      user = jwt.decode(token, 'secret');
+      req.user = user;
+      console.log('User ----------------->', req.user);
+      next();
+    } catch (error) {
+      return next(error);
+    }
+  }
+}
