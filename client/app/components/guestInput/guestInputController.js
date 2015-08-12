9a0a79765f1b02e4e0c591f00423d3cbc88b2b0a
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-12 11:35:33 -0700

    (bug) fix bug around adding empty constraints

--- client/app/components/guestInput/guestInputController.js
+++ client/app/components/guestInput/guestInputController.js
@@ -41,17 +41,22 @@ angular.module('seatly.guestInput', [])
 		if ($scope.guest !== '' && $scope.guest !== '') {
 			$scope.addConstraint();
 		}
-
+		// format the data in the form that the server expects
 		var result = {
 			guests: $scope.guests
 		};
+		// for developer use
 		console.dir($scope.guests);
 
     // POST all guests to database
     guestInputFactory.addAllGuests(result)
     .then(function() {
+	    // then find number of tables
+	    // and POST to tables/sort to use algorithm
     	guestInputFactory.sortGuests($scope.peoplePerTable)
     	.then(function() {
+		    // when that's done, redirect user to 
+		    // list page
     		console.log('55, ready to redirect');
     		// redirect to list page
     	})
@@ -63,10 +68,6 @@ angular.module('seatly.guestInput', [])
     	console.log(new Error(err));
     }); // end of catch from addAllGuests
 
-    // then find number of tables
-    // and POST to tables/sort to use algorithm
-    // when that's done, redirect user to 
-    // list page
 	};
 
 	// add final guest and switch views
@@ -84,10 +85,10 @@ angular.module('seatly.guestInput', [])
 		// For 2 dropdowns: Guest in col 1 is 'guest', guest in col 2 is 'enemy'
     // access the constraints array of the guest
     // if the enemy hasn't already been added, add it
-    if ($scope.guest.constraints.indexOf($scope.enemy.guestName) === -1) {
+    if ($scope.guest && $scope.guest.constraints.indexOf($scope.enemy.guestName) === -1) {
 			$scope.guest.constraints.push($scope.enemy.guestName);
     }
-		if ($scope.enemy.constraints.indexOf($scope.guest.guestName) === -1) {
+		if ($scope.enemy && $scope.enemy.constraints.indexOf($scope.guest.guestName) === -1) {
 			// To create bi-directional constraint listings
 			// access the constraints array of the enemy
 			$scope.enemy.constraints.push($scope.guest.guestName);
