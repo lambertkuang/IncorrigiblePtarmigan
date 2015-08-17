1a60668f2d3a460cd9d16b3555d60ec71eeda6b0
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-17 11:10:14 -0700

    (refactor) build out structure for removing constraints

--- client/app/components/guestList/listFactory.js
+++ client/app/components/guestList/listFactory.js
@@ -1,5 +1,5 @@
 angular.module('seatly.list')
-.factory('List', [ '$http', function($http) {
+.factory('List', [ '$http', '$q', function($http, $q) {
   var viewGuests = function() {
     return $http({
       method: 'GET',
@@ -23,7 +23,7 @@ angular.module('seatly.list')
   var editGuest = function(guestObj) {
     return $http({
       method: 'PUT',
-      url: '/guest/create',
+      url: '/guest/edit',
       data: guestObj
     });
   };
@@ -31,15 +31,55 @@ angular.module('seatly.list')
 // test object: { "changes": [ "Jennie Kim", { "guestName": "JK", "friendName": "Eric"} ]}
 // note: this function doesn't seem very DRY, but hopefully functional
   var deleteGuest = function(guestObj) {
-    // first remove any +1 ties they had
-    if (guestObj.friendName) {
-      return $http({
-        method: 'PUT',
-        url: '/guest/edit',
-        data: {'changes': [guestObj.friendName, { 'friendName' : ''}]}
-      }) // then delete them
-      .then(function() {
-        // delete the guest
+    // first, remove any enemy ties they had
+    return $q(function(resolve, reject) {
+      for (var i = 0; i < guestObj.constraints.length; i++) {
+        getGuest(guestObj.constraints[i])
+        .then(function(constrObj) {
+          var guestIndex = constrObj.data.constraints.indexOf(guestObj.guestName);
+          // this should always be fine but just in case:
+          if (guestIndex !== -1) {
+            constrObj.data.constraints.splice(guestIndex, 1);
+            var changedObj = {'changes': [constrObj.guestName, {'constraints': constrObj.constraints}]};
+
+            editGuest(changedObj)
+            .then(function() {
+              console.log('successfully changed contraint obj');
+            })
+            .catch(function(err) {
+              console.log(new Error(err));
+            }); // end of editGuest
+          }
+        })
+        .catch(function(err) {
+          console.log(new Error(err));
+        }); // end of getGuest
+      }
+    }) // end of q function
+    .then(function(res) {  // then remove any +1 ties they had
+      if (guestObj.friendName) {
+        return $http({
+          method: 'PUT',
+          url: '/guest/edit',
+          data: {'changes': [guestObj.friendName, { 'friendName' : ''}]}
+        })
+        .then(function() {
+          // delete the guest
+          return $http({
+            method: 'POST',
+            url: '/guest/delete',
+            data: {'name': guestObj.guestName}
+          })
+          .then(function() {
+          })
+          .catch(function(err) {
+            console.log(new Error(err));
+          });
+        })
+        .catch(function(err) {
+          console.log(new Error(err));
+        })
+      } else {
         return $http({
           method: 'POST',
           url: '/guest/delete',
@@ -50,22 +90,11 @@ angular.module('seatly.list')
         .catch(function(err) {
           console.log(new Error(err));
         });
-      })
-      .catch(function(err) {
-        console.log(new Error(err));
-      })
-    } else {
-      return $http({
-        method: 'POST',
-        url: '/guest/delete',
-        data: {'name': guestObj.guestName}
-      })
-      .then(function() {
-      })
-      .catch(function(err) {
-        console.log(new Error(err));
-      });
-    }
+      }
+    }, function(err) {
+      console.log(new Error(err));
+    });
+
   };
 
   return {
